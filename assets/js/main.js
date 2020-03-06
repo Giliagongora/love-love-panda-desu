74ab3511d6d2a73b8e077f13ee0b98acbf529d05
“Gilia <giliagongora.g@gmail.com>
2020-03-05 22:04:26 -0300

    actualización

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var ano = new Date();
+var f = '<h3>Love Panda '  +  '/' + ( ano.getFullYear()) + '</h3>';
+document.getElementById("end").innerHTML = f ;
+
+
